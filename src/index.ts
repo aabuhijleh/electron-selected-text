c0325335a17fcea6b0a9b6d7c4b962bbb47b111f
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2020-10-25 12:05:09 +0200

    feat: implement getSelectedText

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { keyTap } from "robotjs";
+import { clipboard } from "electron";
+
+export const getSelectedText = async (): Promise<string> => {
+  const oldClipboardContent = clipboard.readText(); // preserve clipboard content
+  clipboard.clear();
+  keyTap("c", process.platform === "darwin" ? "command" : "control");
+  await new Promise((resolve) => setTimeout(resolve, 200)); // add a delay before checking clipboard
+  const selectedText = clipboard.readText();
+  clipboard.writeText(oldClipboardContent);
+  return selectedText;
+};
