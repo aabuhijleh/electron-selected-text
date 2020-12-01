2c5dfcbb73fb34cbcfb759014c0ea317b1d257a7
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2020-12-01 10:34:16 +0200

    feat: add registerShortcut API

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 import { keyTap } from "robotjs";
-import { clipboard } from "electron";
+import { clipboard, globalShortcut } from "electron";
 
 export const getSelectedText = async () => {
-  const oldClipboardContent = clipboard.readText(); // preserve clipboard content
+  const currentClipboardContent = clipboard.readText(); // preserve clipboard content
   clipboard.clear();
   keyTap("c", process.platform === "darwin" ? "command" : "control");
   await new Promise((resolve) => setTimeout(resolve, 200)); // add a delay before checking clipboard
   const selectedText = clipboard.readText();
-  clipboard.writeText(oldClipboardContent);
+  clipboard.writeText(currentClipboardContent);
   return selectedText;
+};
+
+export const registerShortcut = (
+  accelerator: Electron.Accelerator,
+  callback: (selectedText: string) => any
+) => {
+  const ret = globalShortcut.register(accelerator, async () => {
+    callback(await getSelectedText());
+  });
+
+  if (!ret) {
+    throw new Error("registration failed");
+  }
+};
+
+export const unregisterShortcut = (accelerator: Electron.Accelerator) => {
+  globalShortcut.unregister(accelerator);
 };
